24855804a7a7f8aeaa2bbff699928025a98b5b10
billowbashir <billowbashir@gmail.com>
2022-03-11 12:49:37 -0800

    evaluate random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
-#include <stdlib.h>
+#include <stdlib.>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - assigns a random number to int n everytime
+ * main - assigns a random number to int n everytime 
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
