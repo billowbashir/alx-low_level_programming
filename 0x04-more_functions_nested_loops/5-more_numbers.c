ce8f7077cb3aee26c37f72fb333e0949bc8d9e9c
billowbashir <billowbashir@gmail.com>
2022-03-18 00:01:27 +0300

    nested functions again

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * more_numbers - prints 10x the numbers,
- * from 0 to 14,
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
 void more_numbers(void)
@@ -13,11 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j > 9)
-			{
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			}
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
