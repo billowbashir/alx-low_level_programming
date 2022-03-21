df84aa1c460fcf1d070fa516f554e23991ea15f7
billowbashir <billowbashir@gmail.com>
2022-03-22 00:57:04 +0300

    fix compile errors

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,11 +6,12 @@
  *and changes value it points
  *to
  */
+void reset_to_98(int *n)
+    {
+        *n=98;
+    }
 int main(void)
 {
-    void reset_to_98(int *n)
-    {
-        *n=98
-    }
+
     return (0);
 }
