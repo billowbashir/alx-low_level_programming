efcb5cdd1212f55bd7b20d5b33b3775a23af22c6
billowbashir <billowbashir@gmail.com>
2022-03-22 01:52:41 +0300

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@
 
 void print_rev(char *s)
 {
-int a,b;
+int a, b;
 int len = 0;
 
 for (a = 0; s[a] != '\0'; a++)
 {
 len++;
 }
-for (b= len - 1; b >= 0; b--)
+for (b = len - 1; b >= 0; b--)
 {
 _putchar(s[b]);
 }
