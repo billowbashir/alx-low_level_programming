bc04d35c4481e7f16cd17da857442a8720fa4862
billowbashir <billowbashir@gmail.com>
2022-03-22 00:52:54 +0300

    function to change value via pinter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *function to take pointer as arguement
+ *and changes value it points
+ *to
+ */
+int main(void)
+{
+    void reset_to_98(int *n)
+    {
+        *n=98
+    }
+    return (0);
+}
