771e4f41edfd9c6ded2e4b3650a30f7b8584a782
billowbashir <billowbashir@gmail.com>
2022-03-29 01:39:53 +0300

    fix bugs

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,25 +7,24 @@
  * Return: pointer
  */
 
-	char *_strstr(char *haystack, char *needle)
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+for (i = 0; haystack[i] > '\0'; i++)
+{
+	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
 	{
-		int i, j;
-
-		for (i = 0; haystack[i] > '\0'; i++)
+		if (haystack[j] != needle[j - i])
 		{
-		for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
-		{
-			if (haystack[j] != needle[j - i])
-			{
-				break;
-			}
+			break;
 		}
-		if (needle[j - i] == '\0')
-		{
-			return (haystack + i);
-		}
-		}
-	return (0);
 	}
-
-	
+	if (needle[j - i] == '\0')
+	{
+		return (haystack + i);
+	}
+}
+return (0);
+}
