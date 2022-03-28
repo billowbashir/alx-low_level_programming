0848b6acc37fba154341d24b7da2e59db28030af
billowbashir <billowbashir@gmail.com>
2022-03-29 01:03:18 +0300

    tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - char s char c
+ * @s: string
+ * @c: char
+ *
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+
+	for (; *s != '\0' ; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (*s != c && *s >= c)
+		{
+			return ('\0');
+		}
+	}
+	return (s);
+}
