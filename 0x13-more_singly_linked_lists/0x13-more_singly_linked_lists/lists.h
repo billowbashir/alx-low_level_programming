3cbb1dc358dd7b5671349382cd7c79d8a03c1ad2
billowbashir <billowbashir@gmail.com>
2022-04-26 21:05:51 +0300

    more singly lists

--- 0x13-more_singly_linked_lists/0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/0x13-more_singly_linked_lists/lists.h
@@ -10,7 +10,9 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for the project
  */
+
 typedef struct listint_s
 {
 	int n;
@@ -33,4 +35,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+#endif
